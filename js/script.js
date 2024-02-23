90bbee2733d8f2fc42dfbbeecb3de414d720ceeb
Guilherme Abreu Carvalho <guilhermeabreucarvalho2000@gmail.com>
2024-02-23 20:51:48 -0300

    add

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
-const phone_number_contact = document.getElementById('phone_number_contact') // Seletor do campo de telefone
 
-phone_number_contact.addEventListener('keypress', (e) => phoneMask(e.target.value)) // Dispara quando digitado no campo
-phone_number_contact.addEventListener('change', (e) => phoneMask(e.target.value)) // Dispara quando autocompletado o campo
-
-const phoneMask = (valor) => {
-    valor = valor.replace(/\D/g, "")
-    valor = valor.replace(/^(\d{2})(\d)/g, "($1) $2")
-    valor = valor.replace(/(\d)(\d{4})$/, "$1-$2")
-    phone_number_contact.value = valor // Insere o(s) valor(es) no campo
-}
+const handlePhone = (event) => {
+    let input = event.target
+    input.value = phoneMask(input.value)
+  }
+  
+  const phoneMask = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g,'')
+    value = value.replace(/(\d{2})(\d)/,"($1) $2")
+    value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+    return value
+  }
